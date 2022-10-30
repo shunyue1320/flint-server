44fb5f59b94953789afd8b815ea62b147eaea2d0
shunyue1320 <tsunyue1320@gmail.com>
2022-10-30 22:04:59 +0800

    chore: init project

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'printWidth': 100,
+    'tabWidth': 4,
+    'useTabs': false,
+    'semi': true,
+    'arrowParens': 'avoid',
+    'trailingComma': 'all',
+    'bracketSpacing': true,
+    'overrides': [{
+        'files': '*.yaml',
+        'options': {
+            'tabWidth': 2
+        }
+    }]
+}
