b7a488c95771e38f425488488e80ce09d8643f4a
shunyue1320 <tsunyue1320@gmail.com>
2022-11-01 22:40:51 +0800

    chore: init fastify

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-var a = 1
-console.log(a)
+import fastify from "fastify";
+
+const app = fastify({
+    caseSensitive: true,
+});
+
+app.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+        process.exit(1);
+    }
+    console.log(`服务器启动成功，${address}`);
+});
