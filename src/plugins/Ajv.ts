f865815acaf96c7451b19530949f25f9d15838b6
shunyue1320 <tsunyue1320@gmail.com>
2022-11-13 22:55:42 +0800

    chore: add ajv 'uuid-v4' Format

--- src/plugins/Ajv.ts
+++ src/plugins/Ajv.ts
@@ -1,5 +1,6 @@
 import Ajv, { FormatDefinition } from "ajv";
 import { parsePhoneNumber } from "awesome-phonenumber";
+import { validate as uuidValidate, version as uuidVersion } from "uuid";
 
 const phone: FormatDefinition<string> = {
     validate: (phone: string) => {
@@ -11,7 +12,14 @@ const phone: FormatDefinition<string> = {
     },
 };
 
+const uuidV4: FormatDefinition<string> = {
+    validate: uuid => {
+        return uuidValidate(uuid) && uuidVersion(uuid) === 4;
+    },
+};
+
 export const ajvSelfPlugin = (ajv: Ajv): void => {
     // https://ajv.js.org/guide/formats.html#user-defined-formats
     ajv.addFormat("phone", phone);
+    ajv.addFormat("uuid-v4", uuidV4);
 };
