fa0a22ba150c2cc4626d355ac169a2478562e485
shunyue1320 <tsunyue1320@gmail.com>
2022-11-26 23:20:29 +0800

    chore: ajv self plugin "unix-timestamp"

--- src/plugins/Ajv.ts
+++ src/plugins/Ajv.ts
@@ -1,5 +1,6 @@
 import Ajv, { FormatDefinition } from "ajv";
 import { parsePhoneNumber } from "awesome-phonenumber";
+import { isValid } from "date-fns/fp";
 import { validate as uuidValidate, version as uuidVersion } from "uuid";
 
 const phone: FormatDefinition<string> = {
@@ -18,8 +19,21 @@ const uuidV4: FormatDefinition<string> = {
     },
 };
 
+const unixTimestamp: FormatDefinition<number> = {
+    type: "number",
+    validate: date => {
+        // 必须是毫秒时间戳
+        if (String(date).length !== 13) {
+            return false;
+        }
+
+        return isValid(date);
+    },
+};
+
 export const ajvSelfPlugin = (ajv: Ajv): void => {
     // https://ajv.js.org/guide/formats.html#user-defined-formats
     ajv.addFormat("phone", phone);
     ajv.addFormat("uuid-v4", uuidV4);
+    ajv.addFormat("unix-timestamp", unixTimestamp);
 };
